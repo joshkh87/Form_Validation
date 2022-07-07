306da92d4b9bc502e9837160efa2a8c534d7ccaf
joshkh87 <67205600+joshkh87@users.noreply.github.com>
2022-07-07 11:27:39 -0400

    Update valid.js

--- valid.js
+++ valid.js
@@ -1,7 +1,7 @@
 const name = document.getElementById("name");
 const password = document.getElementById("password");
 const form = document.getElementById("form");
-const errorElement = (document.getElementById("error").style.color = "red");
+const errorElement = document.getElementById("error");
 
 form.addEventListener("submit", (e) => {
   let messages = [];
@@ -24,4 +24,7 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
     errorElement.innerText = messages.join(", ");
   }
+});
+setTimeout(function () {
+  window.location = "http://www.google.com/";
 });
