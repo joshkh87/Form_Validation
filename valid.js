15e91dad59cba64181eb4080fb0840bd09d91641
joshkh87 <67205600+joshkh87@users.noreply.github.com>
2022-07-07 10:50:32 -0400

    Update valid.js

--- valid.js
+++ valid.js
@@ -1,35 +1,38 @@
-const name = document.getElementById('name');
-const password = document.getElementById('password');
-const form = document.getElementById('form');
-const errorElement = document.getElementById('error');
+const name = document.getElementById("name");
+const password = document.getElementById("password");
+const form = document.getElementById("form");
+const errorElement = (document.getElementById("error").style.color = "red");
 
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
   let messages = [];
   if (firstName.value === "" || firstName.value == null) {
     messages.push("Name is Required");
   }
-  if (password.value ==='' || password.value == null) {
-    messages.push('Please Enter A Valid Password! ')
+  if (password.value === "" || password.value == null) {
+    messages.push("Please Enter A Valid Password! ");
   }
-    if (password.value.length <= 6) {
-    messages.push('Password Must Be Longer Than 6 Characters');
+  if (password.value.length <= 6) {
+    messages.push("Password Must Be Longer Than 6 Characters");
   }
   if (password.value.length > 20) {
-    messages.push('Password Must Be Longer Than 6 Characters');
+    messages.push("Password Must Be Longer Than 6 Characters");
   }
-  if (password.value === 'password') {
-    messages.push('Password Cannot Be Password')
+  if (password.value === "password") {
+    messages.push("Password Cannot Be Password");
   }
   if (messages.length > 0) {
     e.preventDefault();
-    errorElement.innerText = messages.join(', ');
+    errorElement.innerText = messages.join(", ");
   }
 });
-function checkform(){
-  if(document.getElementById("un").value == 'jayem30' && document.getElementById("pw").value == 'jayem' ){
-      alert("Login Successful");
-      window.location = "http://www.google.com/"
-  }else{
-      alert("Access denied. Valid username and password is required.");
+function checkform() {
+  if (
+    document.getElementById("un").value == "jayem30" &&
+    document.getElementById("pw").value == "jayem"
+  ) {
+    alert("Login Successful");
+    window.location = "http://www.google.com/";
+  } else {
+    alert("Access denied. Valid username and password is required.");
   }
 }
